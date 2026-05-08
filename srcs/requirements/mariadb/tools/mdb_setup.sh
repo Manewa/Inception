7faf52c805fa9f62f968c939e6528db4f48013ac
namalier <namalier@f6r6s1.paris.42.school>
2026-05-08 19:15:45 +0200

    ff

--- srcs/requirements/mariadb/tools/mdb_setup.sh
+++ srcs/requirements/mariadb/tools/mdb_setup.sh
@@ -10,7 +10,6 @@ fi
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	echo "Initializing mariadb"
 
-	mkdir /var/lib/mysql
 	mysqld_safe --datadir=/var/lib/mysql &
 
 	until mariadb -e "SELECT 1;" > /dev/null 2>&1; do
