b84d27dd52c342bdca6e5063644345eb379bc1cf
namalier <namalier@f6r6s1.paris.42.school>
2026-05-08 17:13:32 +0200

    Readme + tab correction in mariadb

--- srcs/requirements/mariadb/tools/mdb_setup.sh
+++ srcs/requirements/mariadb/tools/mdb_setup.sh
@@ -22,7 +22,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 	GRANT ALL PRIVILEGES ON \`${MYSQL_DB}\`.* TO '${MYSQL_USER}'@'%';
 	FLUSH PRIVILEGES;
-	EOF
+EOF
 
 	mysqladmin shutdown
 
